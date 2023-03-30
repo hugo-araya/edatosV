0673a8b10c94665b9d7fcc73697729b0f869a14e
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2023-03-30 20:22:10 -0300

    Create eje_01.c

---
+++ Clase_03/eje_01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int multiplicar(int a,int b);
+int suma(int a,int b);
+
+int main(){ /* Realiza una suma */
+    int a=10,b=25,t;
+    t=suma(a,b); /* guardamos el valor */
+    printf("%d = %d\n",suma(a,b),t);
+    suma(a,b); /* el valor se pierde */
+    return 0;
+}
+
+int multiplicar(int a,int b){
+    suma(a, b);
+    return (a*b);
+}
+
+int suma(int a,int b){
+    multiplicar(a, b);
+    return (a+b);
+}
