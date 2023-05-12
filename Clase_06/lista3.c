604c83a59d09a8ff6765ebe314dab5e7253efbd8
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2023-05-11 20:55:28 -0400

    Update lista3.c

--- Clase_06/lista3.c
+++ Clase_06/lista3.c
@@ -7,6 +7,8 @@ struct lista{
 };
 
 void crea_lista(struct lista **L);
+void add_inicio(struct lista **L, int elem);
+void recorre(struct lista **L);
 
 int main(){
     // Se crean los punteros a dos listas.
@@ -15,27 +17,38 @@ int main(){
     int i, suma;
     crea_lista(&L); // Se inicializa a NULL el puntero a la lista principal
     for (i = 4; i >= 1; i--){
-        // Reserva memoria para un nodo 
-        p = (struct lista *) malloc(sizeof(struct lista));
-        p->clave = i; // Agrega la informacion 
-        p->sig = L; // Reorganiza la lista 
-        L = p;      // ACtualiza los enlaces
-    }
-
-    // Recorremos la lista y de paso sumamos el valor
-    // de los datos almacenados en ella
-    p = L;
-    suma = 0;
-    while (p != NULL) {
-        printf("%d, ", p->clave);
-        suma = suma + p->clave;
-        p = p->sig;
+        add_inicio(&L, i);
+        printf("%d ", i);
     }
     printf("\n");
+    // Recorremos la lista y de paso sumamos el valor
+    // de los datos almacenados en ella
+    recorre(&L);
     return 0;
 }
 
 void crea_lista(struct lista **L){
     *L = (struct lista *) malloc(sizeof(struct lista)); 
     (*L)->sig = NULL;
+}
+
+void add_inicio(struct lista **L, int elem){
+    struct lista *p;
+    p = (struct lista *) malloc(sizeof(struct lista));
+    p->clave = elem; // Agrega la informacion 
+    p->sig = *L; // Reorganiza la lista 
+    *L = p;   
+}
+
+void recorre(struct lista **L){
+    struct lista *p;
+    int suma;
+    p = *L;
+    suma = 0;
+    while (p->sig != NULL) {
+        printf("%d, ", p->clave);
+        suma = suma + p->clave;
+        p = p->sig;
+    }
+    printf("\n");
 }
